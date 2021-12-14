9303e741209cbb1861c5eb5250b08e86df77c3b0
Arsen <westwest1488@gmail.com>
2021-12-14 23:45:48 +0200

    homework_5update1.1

--- HW5/script.js
+++ HW5/script.js
@@ -3,10 +3,27 @@
 
 //  { word: 'smth', length: 1, isCapitalized: false}
 
-let str = 'Введите текст'
-function myFunk(sting)
+let str = prompt("Введите текст");
+let arrStr = str.split(" ");
+function initialIsCapital(word) {
+  return word[0] !== word[0].toLowerCase();
+}
+function resultFunc(word) {
+  let result = arrStr.map((item) => {
+    if (item === "") {
+      return;
+    }
+    return {
+      word: item,
+      length: item.length,
+      isCapitalised: initialIsCapital(item),
+    };
+  });
+  console.log(result);
+}
+resultFunc(arrStr);
 
-// 2) Реализовать функцию changeFour, которая копирует массив, у копии удаляет 4й элемент и заменяет его строкой 'JavaScript'
+// // 2) Реализовать функцию changeFour, которая копирует массив, у копии удаляет 4й элемент и заменяет его строкой 'JavaScript'
 
 let array = [1, 2, 3, 4, 5];
 console.log(array); // [1, 2, 3, 4, 5];
@@ -16,7 +33,7 @@ function changeFour(arr) {
 }
 console.log(array); // [1, 2, 3, 'JavaScript', 5];
 
-// // 3) Реализовать функцию calcSum, которая вернет сумму элементов массива. Добавить в функцию проверку что значение число.
+// // // 3) Реализовать функцию calcSum, которая вернет сумму элементов массива. Добавить в функцию проверку что значение число.
 
 let sum1 = calcSum([]); // 0
 let sum2 = calcSum([1, 2, 3]); // 6
